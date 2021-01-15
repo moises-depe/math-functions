d9a6caf204cc5ffa0ae98ec88a7a05d56e98f463
moises depena <swe.moisesdepe@gmail.com>
2021-01-15 15:41:07 -0800

    updated2

--- src/math-functions.js
+++ src/math-functions.js
@@ -22,7 +22,7 @@ export function sum(a, b) {
     
 }
 
-
+//
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 // Don't forget to create a new branch for your work on the next question!
 
